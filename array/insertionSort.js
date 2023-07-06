63bc6092d67acb4e43956e589ffdd7f79c54019a
Shone Binu <shonebinualias@gmail.com>
2023-07-06 19:25:28 +0530

    insertion sort

--- array/insertionSort.js
+++ array/insertionSort.js
@@ -1,2 +1,20 @@
 const array = [10, 6, 5, 22, 99, 103, 246];
 
+function insertionSort() {
+    let j, temp;
+    for (let i = 1; i < array.length; i++) {
+        temp = array[i];
+        j = i - 1;
+        while (array[j] > temp && j >= 0) {
+            array[j + 1] = array[j];
+            j--;
+        }
+        array[j + 1] = temp;
+    }
+}
+
+insertionSort();
+
+console.log("Sorted array: ");
+
+array.forEach(item => console.log(item));
