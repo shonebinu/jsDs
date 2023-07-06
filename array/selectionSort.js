655ab2861e487c6d8af5ef274e4df0274ec38663
Shone Binu <shonebinualias@gmail.com>
2023-07-06 18:44:21 +0530

    selection sort

--- array/selectionSort.js
+++ array/selectionSort.js
@@ -1,16 +1,20 @@
 const array = [10, 6, 5, 22, 99, 103, 246];
 
 function selectionSort() {
-    let small, loc, temp;
+    let min_index;
     for (let i = 0; i < array.length - 1; i++) {
-        small = array[i], loc = i;
+        min_index = i;
         for (let j = i + 1; j < array.length; j++) {
-            if (array[j] < small) small = array[j], loc = j;
+            if (array[j] < array[min_index]) min_index = j;
         }
-        temp = array[i];
-        array[i] = array[loc];
-        array[loc] = temp;
+        swap(i, min_index);
     }
+}
+
+function swap(i, j) {
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
 }
 
 selectionSort(array);
